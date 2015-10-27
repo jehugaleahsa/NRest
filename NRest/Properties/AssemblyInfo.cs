ca4635cc3da2e64486da9cf049082853122c0dfd
Travis Parks <jehugaleahsa@gmail.com>
2015-10-27 01:03:10 -0400

    Update assembly for NuGet

--- NRest/Properties/AssemblyInfo.cs
+++ NRest/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NRest")]
-[assembly: AssemblyDescription("Make REST API calls using a fluent syntax.")]
+[assembly: AssemblyDescription("A simple REST client for making API calls using a fluent syntax.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Truncon")]
 [assembly: AssemblyProduct("NRest")]
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ef25bcfd-d3d9-43d6-b3c0-9db02924b672")]
-[assembly: AssemblyVersion("0.0.0.13")]
-[assembly: AssemblyFileVersion("0.0.0.13")]
+[assembly: AssemblyVersion("0.0.0.15")]
+[assembly: AssemblyFileVersion("0.0.0.15")]
 [assembly: CLSCompliant(true)]
