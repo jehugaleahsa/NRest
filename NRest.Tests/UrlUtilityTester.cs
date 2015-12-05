612005dfc8c0dbc678861a465cb7dc5cfc1d8a94
jehugaleahsa <jehugaleahsa@gmail.com>
2015-12-05 08:30:02 -0500

    Write test to confirm MS URL decoding works as expected.

---
+++ NRest.Tests/UrlUtilityTester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NRest.Tests
+{
+    [TestClass]
+    public class UrlUtilityTester
+    {
+        /// <summary>
+        /// This test verifies that even though Microsoft doesn't encode
+        /// spaces and other punctuation exactly to the specification,
+        /// it still decodes correctly.
+        /// </summary>
+        [TestMethod]
+        public void ShouldDecodeSpace()
+        {
+            string url = "Hello%20World%21";
+            string decoded = WebUtility.UrlDecode(url);
+            Assert.AreEqual("Hello World!", decoded);
+        }
+    }
+}
